10c52e9fb3c9ecf28c10907ed36d823ee190a13e
Tzu-Chiao Lin <joseph.tclin1998@gmail.com>
2023-11-19 02:57:36 +0100

    Read correctly but need logic change.

--- Src/Parse_with_line.c
+++ Src/Parse_with_line.c
@@ -17,7 +17,7 @@ int main() {
     struct DataGPS navData;
     int numSats = 0;
     double rinex_version;
-    int cnt=0;
+    int cnt=1;
 
     // Replace with the path to your RINEX navigation file
     const char* filePath = "Data/0lov033b.04n";
@@ -38,17 +38,52 @@ int main() {
 
     // Read data lines
     while (fgets(line, sizeof(line), file) != NULL) {
+        double data1, data2, data3, data4;
+        double data5, data6, data7, data8, data9, data10;
         // Extract data as needed
         replaceDWithE(line);
-        double data1, data2, data3, data4;
-        if (sscanf(line, "%lf%lf%lf%lf", &data1, &data2, &data3, &data4) == 4) {
+        if(cnt == 1){
+            if (sscanf(line, "%lf%lf%lf%lf%lf%lf%lf%lf%lf%lf", &data1, &data2, &data3, &data4, &data5, &data6, &data7, &data8, &data9, &data10) == 10) {
             // Process the extracted data
+            printf("print line = %d\n",cnt);
             printf("print line = %s\n",line);
-            printf("Data: %lf %lf %lf %lf\n", data1, data2, data3, data4);
-        } else {
-            // Handle the case where the line does not match the expected format
-            printf("Error reading data from line: %s", line);
+            // Assign value
+            navData.PRN = data1;
+            navData.clockBias = data8;
+            navData.clockDrift = data9;
+            navData.clockDriftRate = data10;
+            printf("Data: %d %.19lf %.19lf %.19lf\n", navData.PRN, data8, data9, data10);
+            cnt++;
+            } else {
+                // Handle the case where the line does not match the expected format
+                printf("Error reading data from line: %s", line);
+            }
         }
+        else if(cnt >1 && cnt < 8){
+            if (sscanf(line, "%lf%lf%lf%lf", &data1, &data2, &data3, &data4) == 4) {
+            // Process the extracted data
+            printf("print line = %d\n",cnt);
+            printf("print line = %s\n",line);
+            printf("Data: %.12lf %.12lf %.12lf %.12lf\n", data1, data2, data3, data4);
+            cnt++;
+            } else {
+                // Handle the case where the line does not match the expected format
+                printf("Error reading data from line: %s", line);
+            }
+        }
+        else if( cnt == 8){
+            if (sscanf(line, "%lf", &data1) == 1) {
+            // Process the extracted data
+            printf("print line = %d\n",cnt);
+            printf("print line = %s\n",line);
+            printf("Data: %.12lf\n", data1);
+            cnt = 1;
+            } else {
+                // Handle the case where the line does not match the expected format
+                printf("Error reading data from line: %s", line);
+            }
+        }
+        
     }
 
     fclose(file);
