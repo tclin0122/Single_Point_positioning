bc981baad498531c2d0e7860dd7d427db1c0ae57
Tzu-Chiao Lin <joseph.tclin1998@gmail.com>
2023-11-20 19:18:44 +0100

    try to read first line hmmmmmmm

--- Src/Rinex2Obs.c
+++ Src/Rinex2Obs.c
@@ -2,6 +2,15 @@
 #include <stdlib.h>
 #include "Rinex2Obs.h"
 
+void removeG(char *str) {
+    int len = strlen(str);
+    for (int i = 0; i < len; i++) {
+        if (str[i] == 'G') {
+            str[i] = ' ';
+        }
+    }
+}
+
 int rinex2obs(const char* filePath, struct ObsData *obsData, struct ObsHeaderInfo *obsHead) {
     FILE* file;
     char line[MAX_LINE_LENGTH];
@@ -61,11 +70,12 @@ int rinex2obs(const char* filePath, struct ObsData *obsData, struct ObsHeaderInf
         }
     }
 
-    /*
+    
     while (fgets(line, sizeof(line), file) != NULL) {
         double data1, data2, data3, data4;
         double data5, data6, data7, data8, data9, data10;
         int data11;
+        removeG(line);
         if (sscanf(line, "%lf%lf%lf", &data1, &data2, &data3) == 3) {
             // Process the extracted data
             printf("print line = %d\n",cnt);
@@ -73,15 +83,16 @@ int rinex2obs(const char* filePath, struct ObsData *obsData, struct ObsHeaderInf
             // Assign value
             printf("Data: %.12lf %.12lf %.12lf\n", data1, data2, data3);
             cnt++;
-        } else if (strstr(line, "END OF HEADER") != NULL) {
-            break;
-        }
-        else {
+        } else {
             // Handle the case where the line does not match the expected format
             printf("Error reading data from line: %s", line);
         }
+        if (strstr(line, "0.0000000  0") != NULL) {
+            break;
+        }
+        
     }
-    */
+    
 
     // Close the file
     fclose(file);
