fdaa10255c2e082893307dca5a05f545b10b31d4
Tzu-Chiao Lin <joseph.tclin1998@gmail.com>
2023-11-20 22:44:05 +0100

    Reading only first observation data

--- Src/Rinex2Obs.c
+++ Src/Rinex2Obs.c
@@ -15,6 +15,7 @@ int rinex2obs(const char* filePath, struct ObsData *obsData, struct ObsHeaderInf
     FILE* file;
     char line[MAX_LINE_LENGTH];
     int cnt = 1;
+    int prn_index = 1;
 
     // Read file
     file = fopen(filePath, "r");
@@ -27,10 +28,10 @@ int rinex2obs(const char* filePath, struct ObsData *obsData, struct ObsHeaderInf
         double data1;
         if (sscanf(line, "%lf", &data1) <= 1) {
             // Process the extracted data
-            printf("print line = %s\n",line);
+            //printf("print line = %s\n",line);
             // Assign value
             obsHead->rinexType = data1;
-            printf("Data: %lf\n", obsHead->rinexType);
+            printf("Rinex type: %.2f\n", obsHead->rinexType);
             //check rinex file       
             if(obsHead->rinexType > 3 && obsHead->rinexType < 2) {
                 fclose(file);
@@ -49,16 +50,13 @@ int rinex2obs(const char* filePath, struct ObsData *obsData, struct ObsHeaderInf
     while (fgets(line, sizeof(line), file) != NULL) {
         double data1, data2, data3;
         if (sscanf(line, "%lf%lf%lf", &data1, &data2, &data3) == 3) {
-            printf("print line = %s\n",line);
+            //printf("print line = %s\n",line);
             // Assign value
             obsHead -> approxPosX = data1;
             obsHead -> approxPosY = data2;
             obsHead -> approxPosZ = data3;
-            printf("Data: %.4lf %.4lf %.4lf\n", data1, data2, data3);
-        } else {
-            // Handle the case where the line does not match the expected format
-            printf("Error reading data from line: %s", line);
-        } 
+            //printf("Data: %.4lf %.4lf %.4lf\n", data1, data2, data3);
+        }
         if (strstr(line, "APPROX POSITION XYZ") != NULL) {
             break;
         }
@@ -78,12 +76,11 @@ int rinex2obs(const char* filePath, struct ObsData *obsData, struct ObsHeaderInf
         if (sscanf(line, "%lf%lf%lf%lf%lf%lf%lf%d%d%d%d%d%d%d%d%d%d%d%d%d", &data1, &data2, &data3, &data4, &data5, &data6, &data7, &data8,
             &data9[0], &data9[1], &data9[2], &data9[3], &data9[4], &data9[5], &data9[6], &data9[7], &data9[8], &data9[9], &data9[10], &data9[11]) == 20) {
             // Process the extracted data
-            printf("print line = %s\n",line);
+            //printf("print line = %s\n",line);
             // Assign value
             satlist->GPS_num = data8;
             printf("Avaliable Satellite %d\n",satlist->GPS_num);
 
-            printf("Data: %d %d %d\n", data9[9], data9[10], data9[11]);
             for(int i = 0; i < satlist->GPS_num; i++ ) {
                 satlist->PRN_list[i] = data9[i];
                 //printf("show %d\n",satlist->PRN_list[ i ]);
@@ -92,11 +89,36 @@ int rinex2obs(const char* filePath, struct ObsData *obsData, struct ObsHeaderInf
             // Handle the case where the line does not match the expected format
             printf("Error reading data from line: %s", line);
         }
-        
     }
-    
-    
-
+    for(int j=0; j< 3*(satlist->GPS_num); j++) {
+        if (fgets(line, sizeof(line), file) != NULL) {
+        double data1, data2, data3, data4, data5, data6, data7;
+        switch (cnt) {
+            case 1:
+                if (sscanf(line, "%lf%lf%lf%lf%lf%lf%lf", &data1, &data6, &data2, &data7, &data3, &data4, &data5) == 7) {
+                    //printf("print line = %s\n",line);
+                    obsData[prn_index].C1 = data1;
+                    obsData[prn_index].L1 = data2;
+                    obsData[prn_index].L2 = data3;
+                    obsData[prn_index].P1 = data4;
+                    obsData[prn_index].P2 = data5;
+                    cnt++;
+                }
+                break;
+            case 2:
+                if (sscanf(line, "%lf%lf", &data6, &data7) == 2) {
+                    //printf("print line = %s\n",line);
+                    obsData[prn_index].D1 = data6;
+                    obsData[prn_index].D2 = data7;
+                    cnt = 1;
+                    prn_index++;
+                }
+                break;
+            default:
+                break;
+            }
+        }
+        }
     // Close the file
     fclose(file);
     return 0;
