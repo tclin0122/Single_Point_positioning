06500bb5a76d84495d655286c7e5964fe50258b6
Tzu-Chiao Lin <joseph.tclin1998@gmail.com>
2023-11-22 19:35:07 +0100

    Clean unnecessary print log

--- Src/SPP.c
+++ Src/SPP.c
@@ -55,13 +55,9 @@ int spp(struct DataGPS *navData, struct ObsData *obsData, struct ObsHeaderInfo *
             A = pow(navData[satlist->PRN_list[index]].Sqrt_a,2);
             printf("A = %.19lf\n",A);
             mean_motion = sqrt(mu/pow(A,3));
-            printf("mean_motion = %.19lf\n",mean_motion);
             corrected_mean_motion = mean_motion + navData[satlist->PRN_list[index]].Delta_n;
-            printf("corrected_mean_motion = %.19lf\n",corrected_mean_motion);
             tk = system_transmission_time - navData[satlist->PRN_list[index]].TOE;
-            //printf("tk = %.19lf\n",tk);
             Mean_anomaly = navData[satlist->PRN_list[index]].Mo + corrected_mean_motion * tk;
-            //printf("Mean_anomaly = %.19lf\n",Mean_anomaly);
             E0 = Mean_anomaly;
             err_Ek = 1;
             while (fabs(err_Ek) > err_tol) {
@@ -70,46 +66,32 @@ int spp(struct DataGPS *navData, struct ObsData *obsData, struct ObsHeaderInfo *
                 printf("Err = %.19lf\n",err_Ek);
                 E0 = Ek;
             }
-            //printf("Ek = %.19lf\n",Ek);
             relativistic_correction = F * navData[satlist->PRN_list[index]].Eccentricity * navData[satlist->PRN_list[index]].Sqrt_a * sin(Ek);
-            //printf("%.19lf\n",relativistic_correction);
             err_rel -= relativistic_correction;
         }
-        
         //Compute satellite coordinates Xs, Ys, Zs
         a = (sqrt(1.0 - pow(navData[satlist->PRN_list[index]].Eccentricity,2)) * sin(Ek)) / (1.0 - navData[satlist->PRN_list[index]].Eccentricity * cos(Ek));
-        //printf("%lf\n",a);
         b = (cos(Ek) - navData[satlist->PRN_list[index]].Eccentricity) / (1.0 - navData[satlist->PRN_list[index]].Eccentricity * cos(Ek));
-        //printf("%lf\n",b);
         vk = atan2 ( a , b );
-        //printf("%lf\n",vk);
         lat_k = vk + navData[satlist->PRN_list[index]].Omega;
-        //printf("%lf\n",lat_k);
         lat_correction = navData[satlist->PRN_list[index]].Cus * sin(2 * lat_k) + navData[satlist->PRN_list[index]].Cuc * cos(2 * lat_k);
         radi_correction = navData[satlist->PRN_list[index]].Crs * sin(2 * lat_k) + navData[satlist->PRN_list[index]].Crc * cos(2 * lat_k);
         inclin_correction = navData[satlist->PRN_list[index]].Cis * sin(2 * lat_k) +navData[satlist->PRN_list[index]].Cic * cos(2 * lat_k);
         corrected_lat = lat_k + lat_correction;
         corrected_radi = A * (1 - navData[satlist->PRN_list[index]].Eccentricity * cos(Ek)) + radi_correction;
         corrected_inclin = navData[satlist->PRN_list[index]].Io + inclin_correction + navData[satlist->PRN_list[index]].IDOT * tk;
-        //printf("%lf\n",corrected_lat);
         x_in_orb = corrected_radi * cos(corrected_lat);
         y_in_orb = corrected_radi * sin(corrected_lat);
-        //printf("%lf\n",y_in_orb);
         corrected_ascending_node = navData[satlist->PRN_list[index]].OMEGA + (navData[satlist->PRN_list[index]].Omega_dot - Omeg_dot_earth) * tk - Omeg_dot_earth * navData[satlist->PRN_list[index]].TOE;
-        //printf("%lf\n",corrected_ascending_node);
 
         X_s[index] = x_in_orb * cos(corrected_ascending_node) - y_in_orb * cos(corrected_inclin) * sin(corrected_ascending_node);
         Y_s[index] = x_in_orb * sin(corrected_ascending_node) + y_in_orb * cos(corrected_inclin) * cos(corrected_ascending_node);
         Z_s[index] = y_in_orb * sin(corrected_inclin);
-        printf("PRN = %d\n", navData[satlist->PRN_list[index]].PRN);
-        printf("X_s = %lf\n",X_s[index]);
-        printf("Y_s = %lf\n",Y_s[index]);
-        printf("Z_s = %lf\n",Z_s[index]);
         //update the index
         index++;
-        printf("index = %d\n",index);
     }
     index = 0;
+    // show the satellite coord.
     while (index < satlist->GPS_num) {
         printf("PRN = %d\n", navData[satlist->PRN_list[index]].PRN);
         printf("X_s = %lf\n",X_s[index]);
@@ -117,8 +99,14 @@ int spp(struct DataGPS *navData, struct ObsData *obsData, struct ObsHeaderInfo *
         printf("Z_s = %lf\n",Z_s[index]);
         //update the index
         index++;
-        printf("index = %d\n",index);
     }
+    //Trop delay
+
+    //Iono delay
+
+    //Approximate distance
+
+    
    
  
     return 0;
